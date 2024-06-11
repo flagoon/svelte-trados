b45349b9d12c4cd7375fcc531b4495b9145efa4e
Pawel Muody Kochanek <pawel.kochanek@gmail.com>
2024-06-11 21:52:21 +0200

    build: typescript for contentful

---
+++ src/types/TypeBlogPost.ts
@@ -0,0 +1,51 @@
+import type {
+	ChainModifiers,
+	Entry,
+	EntryFieldTypes,
+	EntrySkeletonType,
+	LocaleCode
+} from 'contentful';
+import type { TypeCarouselSkeleton } from './TypeCarousel';
+
+export interface TypeBlogPostFields {
+	name: EntryFieldTypes.Symbol;
+	title: EntryFieldTypes.Symbol;
+	carousel?: EntryFieldTypes.EntryLink<TypeCarouselSkeleton>;
+	heroImage?: EntryFieldTypes.AssetLink;
+	description?: EntryFieldTypes.RichText;
+	autor?: EntryFieldTypes.Symbol;
+	tags?: EntryFieldTypes.Array<
+		EntryFieldTypes.Symbol<
+			| 'RWS-AppStore'
+			| 'edytor'
+			| 'edytor-chmurowy'
+			| 'edytor-klasyczny'
+			| 'pamięci-tłumaczeń'
+			| 'pomoc-techniczna'
+			| 'samouczek'
+			| 'statystyki-i-raporty'
+			| 'szkolenie'
+			| 'sztuczna-inteligencja'
+			| 'terminologia'
+			| 'trados-accelerate'
+			| 'trados-bussines-manager'
+			| 'trados-enterprise'
+			| 'trados-groupshare'
+			| 'trados-language-cloud'
+			| 'trados-podstawy'
+			| 'trados-studio-freelance'
+			| 'trados-studio-professional'
+			| 'trados-team'
+			| 'tłumaczenie-maszynowe'
+			| 'zadania-seryjne'
+		>
+	>;
+	slug: EntryFieldTypes.Symbol;
+	seo?: EntryFieldTypes.Symbol;
+}
+
+export type TypeBlogPostSkeleton = EntrySkeletonType<TypeBlogPostFields, 'blogPost'>;
+export type TypeBlogPost<
+	Modifiers extends ChainModifiers,
+	Locales extends LocaleCode = LocaleCode
+> = Entry<TypeBlogPostSkeleton, Modifiers, Locales>;
---
+++ src/types/TypeRequirements.ts
@@ -0,0 +1,18 @@
+import type {
+	ChainModifiers,
+	Entry,
+	EntryFieldTypes,
+	EntrySkeletonType,
+	LocaleCode
+} from 'contentful';
+
+export interface TypeRequirementsFields {
+	name: EntryFieldTypes.Symbol;
+	requirements: EntryFieldTypes.Object;
+}
+
+export type TypeRequirementsSkeleton = EntrySkeletonType<TypeRequirementsFields, 'requirements'>;
+export type TypeRequirements<
+	Modifiers extends ChainModifiers,
+	Locales extends LocaleCode = LocaleCode
+> = Entry<TypeRequirementsSkeleton, Modifiers, Locales>;
