91ea79bf1f28b3f770ab538f6c67a697c2e96871
Pawel Muody Kochanek <pawel.kochanek@gmail.com>
2024-07-12 23:01:30 +0200

    chore: generate types

--- src/types/TypeStaticContent.ts
+++ src/types/TypeStaticContent.ts
@@ -1,20 +1,23 @@
 import type {
-	ChainModifiers,
-	Entry,
-	EntryFieldTypes,
-	EntrySkeletonType,
-	LocaleCode
-} from 'contentful';
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from "contentful";
 
 export interface TypeStaticContentFields {
-	name: EntryFieldTypes.Symbol;
-	label: EntryFieldTypes.Symbol;
-	textContent: EntryFieldTypes.RichText;
-	slug: EntryFieldTypes.Symbol;
+  name: EntryFieldTypes.Symbol;
+  label: EntryFieldTypes.Symbol;
+  textContent: EntryFieldTypes.RichText;
+  slug: EntryFieldTypes.Symbol;
 }
 
-export type TypeStaticContentSkeleton = EntrySkeletonType<TypeStaticContentFields, 'staticContent'>;
+export type TypeStaticContentSkeleton = EntrySkeletonType<
+  TypeStaticContentFields,
+  "staticContent"
+>;
 export type TypeStaticContent<
-	Modifiers extends ChainModifiers,
-	Locales extends LocaleCode = LocaleCode
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode = LocaleCode,
 > = Entry<TypeStaticContentSkeleton, Modifiers, Locales>;
