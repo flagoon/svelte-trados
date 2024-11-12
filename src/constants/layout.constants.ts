831dacdec234c421290a644f52d49905e960c8c2
Pawel Muody Kochanek <pawel.kochanek@gmail.com>
2024-11-12 20:41:12 +0100

    chore: main layout with navigation

---
+++ src/constants/layout.constants.ts
@@ -0,0 +1,18 @@
+export const MENU_LIST = [
+  {
+    name: "Trados",
+    slug: "/trados",
+  },
+  {
+    name: "Szkolenia",
+    slug: "/szkolenia",
+  },
+  {
+    name: "Blog",
+    slug: "/blog",
+  },
+  {
+    name: "Pomoc",
+    slug: "/pomoc",
+  },
+];
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,26 @@
 <script lang="ts">
-  import { Header } from "$lib/components/app/header";
   import { ModeWatcher } from "mode-watcher";
   import "../app.css";
   import ModeSwitch from "$lib/components/app/mode-switch.svelte";
+  import Logo from "$lib/assets/logo_main_page.svelte";
+
+  import Mail from "lucide-svelte/icons/mail";
+  import { MENU_LIST } from "$constants/layout.constants";
 
   let { children } = $props();
-  const menuList = [
-    {
-      name: "Trados",
-      slug: "/trados",
-    },
-    {
-      name: "Szkolenia",
-      slug: "/szkolenia",
-    },
-    {
-      name: "Blog",
-      slug: "/blog",
-    },
-    {
-      name: "Pomoc",
-      slug: "/pomoc",
-    },
-  ];
 </script>
 
 <ModeWatcher />
-<Header {menuList} />
-{@render children()}
-<footer>footer</footer>
+<nav class="sticky bg-accent">
+  <div class=" border-1 mx-auto flex max-w-7xl items-center gap-8 border-red-500">
+    <a href="/"><Logo /></a>
+    {#each MENU_LIST as { name, slug }}
+      <a class="hover:text-muted-foreground" href={slug}>{name}</a>
+    {/each}
+    <a href="/kontakt" class="ml-auto flex gap-2 p-4 hover:text-muted-foreground"><Mail />Kontakt</a
+    >
+    <ModeSwitch />
+  </div>
+</nav>
+
+<main class="mx-auto max-w-7xl">{@render children()}</main>
